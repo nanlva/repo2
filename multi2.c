35b8e18613bf974014931afff0f8480a70c140e4
rua <1072597914@qq.com>
2023-07-02 17:16:39 +0800

    modify in repo2 again

--- multi2.c
+++ multi2.c
@@ -5,5 +5,6 @@ int main(void)
     printf("hello, this is git multirepo practise");
     printf("this is repo2");
     printf("modify in repo2");
+    printf("modify in repo2 again");
     return 0;
 }
